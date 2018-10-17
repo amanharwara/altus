42639151fa7d5c28a88260dcf28d9e84c1cf1936
ShadyThGod <amanharwara@gmail.com>
2018-10-17 23:40:30 +0530

    Fix Copy/Paste issue on Mac

--- src/js/whatsapp.init.js
+++ src/js/whatsapp.init.js
@@ -1,6 +1,6 @@
 const { ipcRenderer } = require('electron');
 
-var _Notification = window.Notification;
+/* var _Notification = window.Notification;
 var ProxyNotification = function(title, options) {
     var _notification = new _Notification(title, options);
     this.title = _notification.title;
@@ -38,10 +38,24 @@ var ProxyNotification = function(title, options) {
 
 ProxyNotification.permission = _Notification.permission;
 ProxyNotification.requestPermission = _Notification.requestPermission;
-window.Notification = ProxyNotification;
+window.Notification = ProxyNotification; */
 
 window.onclick = function(e) {
     if (e.target.tagName == "A") {
         ipcRenderer.send('linkOpen', e.target.href);
     }
+}
+
+/* Mac Copy/Paste Fix */
+var isCtrl = false;
+var isCmd = false;
+window.onkeyup = function(e) {
+    if (e.which == 17) isCtrl = false;
+    if (e.which == 91) isCmd = false;
+}
+window.onkeydown = function(e) {
+    if (e.which == 17) isCtrl = true;
+    if (e.which == 91) isCmd = false;
+    if (e.which == 67 && (isCtrl || isCmd)) document.execCommand('copy');
+    if (e.which == 86 && (isCtrl || isCmd)) document.execCommand('paste');
 }
