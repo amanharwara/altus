6481be3abeda7022d5a13d0f07704985e8cd901f
ShadyThGod <amanharwara@gmail.com>
2018-12-17 22:06:48 +0530

    Added fix for "WhatsApp Works For Chrome 36+" issue i.e. Issue #7

--- src/js/whatsapp.init.js
+++ src/js/whatsapp.init.js
@@ -1,9 +1,30 @@
-const { ipcRenderer } = require('electron');
+const {
+    ipcRenderer,
+    remote
+} = require('electron');
 const Mousetrap = require('mousetrap');
 window.onclick = function(e) {
     if (e.target.tagName == "A") {
         ipcRenderer.send('linkOpen', e.target.href);
     }
+}
+
+var ses = remote.session.defaultSession;
+
+ses.flushStorageData();
+ses.clearStorageData({
+    storages: ['appcache', 'serviceworkers', 'cachestorage', 'websql', 'indexdb'],
+});
+
+window.navigator.serviceWorker.getRegistrations().then(registrations => {
+    for (let registration of registrations) {
+        registration.unregister();
+    }
+});
+
+const titleEl = document.querySelector('.window-title');
+if (titleEl && titleEl.innerHTML.includes('Google Chrome 36+')) {
+    window.location.reload();
 }
 
 /* Mac Copy/Paste Fix */
