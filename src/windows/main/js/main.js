6c58da8d9b5916334e89742d9f85dbf392b95038
ShadyThGod <amanharwara@gmail.com>
2019-01-16 23:45:30 +0530

    Update main window

--- src/windows/main/js/main.js
+++ src/windows/main/js/main.js
@@ -2,14 +2,16 @@ const {
     Titlebar
 } = require('custom-electron-titlebar');
 const {
-    app
+    app,
+    process
 } = require('electron').remote;
 
 // Create main window titlebar
 const mainTitlebar = new Titlebar('#21252B', {
-    icon: '../../build/icon.ico',
+    icon: '../assets/icons/icon.ico',
     menuItemHoverColor: '#3d444e',
 });
+
 
 // Setting title explicitly
 mainTitlebar.updateTitle(`Altus ${app.getVersion()}`);
