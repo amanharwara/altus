c891ea9f1214280590a7126ab067dd4304842718
ShadyThGod <amanharwara@gmail.com>
2019-01-20 16:56:22 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ const {
 } = require('electron');
 const url = require('url');
 const path = require('path');
-const mainMenuTemplate = require('./js/mainMenuTemplate');
 const Store = require('electron-store');
 
 //Declare window variables
@@ -65,12 +64,187 @@ if (!singleInstanceLock) {
         }));
 
         //Setting main menu
-        const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+        const mainMenu = Menu.buildFromTemplate(template);
         Menu.setApplicationMenu(mainMenu);
+    });
+
+    //Quit app if all windows are closed
+    app.on('window-all-closed', () => {
+        app.quit()
     });
 }
 
-//Quit app if all windows are closed
-app.on('window-all-closed', () => {
-    app.quit()
-});
+const template = [{
+    label: 'File',
+    submenu: [{
+        label: 'Open DevTools',
+        accelerator: 'CmdOrCtrl+Shift+I',
+        click() {
+            var window = BrowserWindow.getFocusedWindow();
+            window.webContents.openDevTools();
+        }
+    }, {
+        label: 'Force Reload',
+        accelerator: 'CmdOrCtrl+Shift+R',
+        click() {
+            var window = BrowserWindow.getFocusedWindow();
+            window.webContents.reload();
+        }
+    }, {
+        label: 'Quit',
+        accelerator: 'CmdOrCtrl+Q',
+        click() {
+            app.quit();
+        }
+    }]
+}, {
+    label: 'Theme',
+    submenu: [{
+        label: 'Change Theme',
+        submenu: [{
+                label: 'Default Theme',
+                accelerator: 'CmdOrCtrl+Shift+D',
+                click() {
+                    console.log("Default Theme")
+                }
+            },
+            {
+                label: 'Dark/Night Theme',
+                accelerator: 'CmdOrCtrl+Shift+N',
+                click() {
+                    console.log("Dark Theme")
+                }
+            }
+        ]
+    }, {
+        label: 'Custom CSS Theme',
+        accelerator: 'CmdOrCtrl+Shift+T',
+        click() {
+            createWindow('customCSS')
+        }
+    }, {
+        label: 'Theme Customizer',
+        accelerator: 'CmdOrCtrl+Alt+T',
+        click() {
+            createWindow('themeCustomizer')
+        }
+    }]
+}, {
+    label: 'Settings',
+    submenu: [{
+        label: 'Settings',
+        accelerator: 'CmdOrCtrl+,',
+        click() {
+            createWindow('settings')
+        }
+    }]
+}, {
+    label: "About",
+    submenu: [{
+        label: "About",
+        click() {
+            createWindow('about')
+        }
+    }]
+}]
+
+function createWindow(id) {
+    switch (id) {
+        // Creates about window
+        case 'about':
+            if (typeof aboutWindow === 'object') {
+                aboutWindow.show()
+            } else {
+                aboutWindow = new BrowserWindow({
+                    title: `About Altus`,
+                    frame: false,
+                    titleBarStyle: 'hidden',
+                    width: 320,
+                    height: 400,
+                    resizable: false,
+                    parent: mainWindow,
+                    modal: true,
+                    webPreferences: {
+                        nodeIntegration: true
+                    }
+                })
+                aboutWindow.loadURL(path.resolve('./windows/about/window.html'));
+                aboutWindow.show();
+                aboutWindow.on('close', e => {
+                    e.preventDefault();
+                    aboutWindow.hide();
+                });
+            }
+            break;
+        case 'settings':
+            if (typeof settingsWindow === 'object') {
+                settingsWindow.show()
+            } else {
+                settingsWindow = new BrowserWindow({
+                    title: `Settings`,
+                    frame: false,
+                    titleBarStyle: 'hidden',
+                    width: 600,
+                    height: 500,
+                    parent: mainWindow,
+                    modal: true,
+                    webPreferences: {
+                        nodeIntegration: true
+                    }
+                })
+                settingsWindow.loadURL(path.resolve('./windows/settings/window.html'));
+                settingsWindow.show();
+                settingsWindow.on('close', e => {
+                    e.preventDefault();
+                    settingsWindow.hide();
+                });
+            }
+            break;
+        case 'customCSS':
+            if (typeof customCSSWindow === 'object') {
+                customCSSWindow.show()
+            } else {
+                customCSSWindow = new BrowserWindow({
+                    title: `Custom CSS for WhatsApp`,
+                    frame: false,
+                    titleBarStyle: 'hidden',
+                    parent: mainWindow,
+                    modal: true,
+                    webPreferences: {
+                        nodeIntegration: true
+                    }
+                })
+                customCSSWindow.loadURL(path.resolve('./windows/customCSS/window.html'));
+                customCSSWindow.show();
+                customCSSWindow.on('close', e => {
+                    e.preventDefault();
+                    customCSSWindow.hide();
+                });
+            }
+            break;
+        case 'themeCustomizer':
+            if (typeof themeCustomizerWindow === 'object') {
+                themeCustomizerWindow.show()
+            } else {
+                themeCustomizerWindow = new BrowserWindow({
+                    title: `Customize Theme`,
+                    frame: false,
+                    titleBarStyle: 'hidden',
+                    parent: mainWindow,
+                    modal: true,
+                    webPreferences: {
+                        nodeIntegration: true
+                    }
+                })
+                themeCustomizerWindow.loadURL(path.resolve('./windows/themeCustomizer/window.html'));
+                themeCustomizerWindow.show();
+                themeCustomizerWindow.on('close', e => {
+                    e.preventDefault();
+                    themeCustomizerWindow.hide();
+                });
+            }
+            break;
+        default:
+            break;
+    }
+}
