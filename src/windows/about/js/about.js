1e9e080aeebd05aac06ba54018c279ae8d2838f4
ShadyThGod <amanharwara@gmail.com>
2019-02-14 00:00:27 +0530

    Update about.js

--- src/windows/about/js/about.js
+++ src/windows/about/js/about.js
@@ -1,17 +1,21 @@
-const {
-    Titlebar
-} = require('custom-electron-titlebar');
+const customTitlebar = require('custom-electron-titlebar');
 const {
     app,
-    process
+    process,
+    Menu
 } = require('electron').remote;
+const {
+    ipcRenderer
+} = require('electron');
 
 // Create main window titlebar
-const mainTitlebar = new Titlebar('#21252B', {
-    icon: null,
-    menu: null,
+const mainTitlebar = new customTitlebar.Titlebar({
+    backgroundColor: customTitlebar.Color.fromHex('#21252B'),
+    icon: '../assets/icons/icon.ico',
+    menu: new Menu(),
     minimizable: false,
-    maximizable: false
+    maximizable: false,
+    closeable: true
 });
 
 document.getElementById('version').innerText = app.getVersion();
@@ -19,5 +23,10 @@ document.getElementById('electron-version').innerText = 'v' + process.versions.e
 document.getElementById('chrome-version').innerText = 'v' + process.versions.chrome;
 document.getElementById('node-version').innerText = 'v' + process.versions.node;
 
+window.onclick = e => {
+    e.preventDefault();
+    if (e.target.tagName == "A") ipcRenderer.send('link-open', e.target.href)
+};
+
 // Setting title explicitly
-mainTitlebar.updateTitle(`About Altus`);
+mainTitlebar.updateTitle(`About Altus`)
