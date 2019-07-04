753084c9dc8f4630cc13a7aeaa97642a6ac32a9f
ShadyThGod <amanharwara@gmail.com>
2019-07-04 22:10:47 +0530

    Update main.js
    Add "Edit" menu for Cut/Copy/Paste/SelectAll shortcuts

--- src/main.js
+++ src/main.js
@@ -275,6 +275,42 @@ const template = [{
     label: 'File',
     submenu: fileMenuTemplate
 }, {
+    label: "Edit",
+    submenu: [{
+            label: "Undo",
+            accelerator: "CmdOrCtrl+Z",
+            selector: "undo:"
+        },
+        {
+            label: "Redo",
+            accelerator: "Shift+CmdOrCtrl+Z",
+            selector: "redo:"
+        },
+        {
+            type: "separator"
+        },
+        {
+            label: "Cut",
+            accelerator: "CmdOrCtrl+X",
+            selector: "cut:"
+        },
+        {
+            label: "Copy",
+            accelerator: "CmdOrCtrl+C",
+            selector: "copy:"
+        },
+        {
+            label: "Paste",
+            accelerator: "CmdOrCtrl+V",
+            selector: "paste:"
+        },
+        {
+            label: "Select All",
+            accelerator: "CmdOrCtrl+A",
+            selector: "selectAll:"
+        }
+    ]
+}, {
     label: 'Theme',
     submenu: [{
         label: 'Custom CSS Theme',
