357d684e45900fc59a7b9b07fcc1bdd18886866c
Aman Harwara <amanharwara@gmail.com>
2020-05-05 22:47:17 +0530

    Update Settings UI + New Features

--- src/windows/settings/settings.js
+++ src/windows/settings/settings.js
@@ -5,7 +5,8 @@ const customTitlebar = require('custom-electron-titlebar');
 const {
     app,
     process,
-    Menu
+    Menu,
+    dialog
 } = require('electron').remote;
 const {
     ipcRenderer
@@ -13,6 +14,13 @@ const {
 
 // Import electron store module for settings
 const Store = require('electron-store');
+
+// Import default settings
+const {
+    defaultSettings
+} = require("../../js/defaultSettings");
+
+const fs = require('fs');
 
 // Load the main settings into settings variable
 let settings = new Store({
@@ -23,7 +31,7 @@ let settings = new Store({
 if (Array.from(settings.get('settings')).find(s => s.id === 'customTitlebar').value === true && process.platform !== 'darwin') {
     // Create main window titlebar
     const mainTitlebar = new customTitlebar.Titlebar({
-        backgroundColor: customTitlebar.Color.fromHex('#1a1821'),
+        backgroundColor: customTitlebar.Color.fromHex('#202224'),
         icon: '../otherAssets/icon.ico',
         itemBackgroundColor: customTitlebar.Color.fromHex('#1c2028'),
         menu: process.platform === 'darwin' ? Menu.getApplicationMenu() : new Menu(),
@@ -46,25 +54,20 @@ if (Array.from(settings.get('settings')).find(s => s.id === 'customTitlebar').va
 // Loop through all the settings (except tray icon on Linux as it is not supported)
 Array.from(process.platform === 'linux' ? settings.get('settings').filter(s => s.id !== 'trayIcon' && s.id !== 'closeToTray') : settings.get('settings')).forEach(setting => {
     // Create setting element for DOM
-    let settingElement = document.createRange().createContextualFragment(`<div class="setting" data-setting="${setting.id}" data-value="${setting.value ? 'enabled' : 'disabled'}"><div class="information"><div class="setting-title">${setting.name}</div><div class="setting-description">${setting.description}</div></div><div id="${setting.id}-toggle" class="setting-toggle ${setting.value ? 'setting-toggle-enabled' : 'setting-toggle-disabled'}">${setting.value ? '<span class="lni-check-mark-circle"></span> Enabled' : '<span class="lni-cross-circle"></span> Disabled'}</div></div>`);
+    let settingElement = document.createRange()
+        .createContextualFragment(`<div class="setting" data-setting="${setting.id}" data-value="${setting.value ? 'enabled' : 'disabled'}">
+        <div class="information">
+            <div class="setting-title">${setting.name}</div>
+            <div class="setting-description">${setting.description}</div>
+        </div>
+        <div class="input-checkbox" id="${setting.id}-toggle">
+            <input type="checkbox" id="${setting.id}-checkbox" class="checkbox" ${setting.value === true ? "checked" : ""}>
+            <div class="toggle-bg"></div>
+        </div>
+    </div>`);
 
     // Append setting element to DOM
     document.querySelector('#settings-container').appendChild(settingElement);
-
-    // Click listener for toggle button
-    document.querySelector(`#${setting.id}-toggle`).addEventListener('click', () => {
-        if (document.querySelector(`#${setting.id}-toggle`).classList.contains('setting-toggle-enabled')) {
-            document.querySelector(`#${setting.id}-toggle`).classList.remove('setting-toggle-enabled');
-            document.querySelector(`#${setting.id}-toggle`).classList.add('setting-toggle-disabled');
-            document.querySelector(`#${setting.id}-toggle`).innerHTML = '<span class="lni-cross-circle"></span> Disabled';
-            document.querySelector(`#${setting.id}-toggle`).parentElement.setAttribute('data-value', 'disabled');
-        } else {
-            document.querySelector(`#${setting.id}-toggle`).classList.add('setting-toggle-enabled');
-            document.querySelector(`#${setting.id}-toggle`).classList.remove('setting-toggle-disabled');
-            document.querySelector(`#${setting.id}-toggle`).innerHTML = '<span class="lni-check-mark-circle"></span> Enabled';
-            document.querySelector(`#${setting.id}-toggle`).parentElement.setAttribute('data-value', 'enabled');
-        }
-    });
 });
 
 // Save settings button
@@ -78,13 +81,13 @@ document.querySelector('.save.button').addEventListener('click', e => {
     // Loop through current settings
     Array.from(process.platform === 'linux' ? settings.get('settings').filter(s => s.id !== 'trayIcon') : settings.get('settings')).forEach(setting => {
         // Find the DOM element of the setting
-        let settingDOMElement = document.querySelector(`[data-setting="${setting.id}"]`);
+        let settingDOMElement = document.querySelector(`#${setting.id}-checkbox`);
 
         // Get new value of the setting
-        let newValue = settingDOMElement.getAttribute('data-value');
+        let newValue = settingDOMElement.checked;
 
         // Set the new value to the current setting object
-        setting.value = newValue === 'enabled' ? true : false;
+        setting.value = newValue === true ? true : false;
 
         // Push the current setting object to the newSettings array
         newSettings.push(setting);
@@ -92,7 +95,7 @@ document.querySelector('.save.button').addEventListener('click', e => {
 
     // Get rid of spinner
     setTimeout(() => {
-        document.querySelector('.save.button').innerHTML = '<span class="lni-save"></span>';
+        document.querySelector('.save.button').innerHTML = 'Save';
     }, 250);
 
     // Set new settings globally
@@ -100,4 +103,70 @@ document.querySelector('.save.button').addEventListener('click', e => {
 
     // Send IPC message to index.js because settings are changed
     ipcRenderer.send('settings-changed', true);
+});
+
+// Reset settings
+document.querySelector('.reset.button').addEventListener('click', () => {
+    if (confirm("Do you want to reset the settings?")) {
+        settings.store = {
+            settings: defaultSettings
+        };
+        ipcRenderer.send('settings-changed', true);
+        window.location.reload();
+    }
+});
+
+// Import settings
+document.querySelector('.import.button').addEventListener('click', () => {
+    dialog.showOpenDialog({
+            title: "Import Settings",
+            filters: [{
+                name: "JSON",
+                extensions: ["json"]
+            }],
+            properties: ['openFile']
+        })
+        .then(result => {
+            if (!result.canceled) {
+                fs.readFile(result.filePaths[0], (err, data) => {
+                    if (!err) {
+                        const imported = JSON.parse(data.toString());
+                        settings.store = imported;
+                        ipcRenderer.send('settings-changed', true);
+                        window.location.reload();
+                    }
+                })
+            }
+        })
+        .catch(err => {
+            if (err) throw err;
+            console.log(err);
+        });
+});
+
+// Export settings
+document.querySelector('.export.button').addEventListener('click', () => {
+    dialog.showSaveDialog({
+            title: "Export Settings",
+            filters: [{
+                name: "JSON",
+                extensions: ["json"]
+            }]
+        })
+        .then(result => {
+            const {
+                filePath,
+                canceled
+            } = result;
+            if (!canceled) {
+                const data = new Uint8Array(Buffer.from(JSON.stringify(settings.store, null, '\t')));
+                fs.writeFile(filePath, data, (err) => {
+                    if (err) throw err;
+                });
+            }
+        })
+        .catch(err => {
+            if (err) throw err;
+            console.log(err);
+        });
 });
