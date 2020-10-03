f9db166e90eb17aa0e049a4854c071e6cbcf3beb
Marcelo <marcelozapatta0@gmail.com>
2020-10-03 09:48:35 -0300

    ADD: toggle for prevent Enter

--- src/index.js
+++ src/index.js
@@ -964,17 +964,28 @@ if (!singleInstanceLock) {
       ],
     });
 
-    c.on('before-input-event', function (event, input) {
-      if (input.key === 'Enter' && !input.shift && !input.control) {
-        c.webContents.sendInputEvent({keyCode: 'Shift+Return', type: 'keyDown'});
-        event.preventDefault()
-        return;
-      }
+    // Prevents default Enter (instead Control + Enter)
+    c.on("before-input-event", function (event, input) {
+      if (
+        settings.get("settings").find((s) => s.id === "preventEnter").value ===
+        true
+      ) {
+        if (input.key === "Enter" && !input.shift && !input.control) {
+          c.webContents.sendInputEvent({
+            keyCode: "Shift+Return",
+            type: "keyDown",
+          });
+          event.preventDefault();
+          return;
+        }
 
-      if (input.key === 'Enter' && input.control) {
-        c.webContents.executeJavaScript(`document.querySelector('[data-icon="send"]').click()`);
-        event.preventDefault()
-        return;
+        if (input.key === "Enter" && input.control) {
+          c.webContents.executeJavaScript(
+            `document.querySelector('[data-icon="send"]').click()`
+          );
+          event.preventDefault();
+          return;
+        }
       }
     });
   });
