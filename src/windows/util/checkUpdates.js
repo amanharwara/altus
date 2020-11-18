554037c57a3c9b5d7af49d3c77ad1e16c5d25e4a
Aman Harwara <amanharwara@protonmail.com>
2020-11-18 13:30:53 +0530

    New check updates dialog

---
+++ src/windows/util/checkUpdates.js
@@ -0,0 +1,53 @@
+const { getReleases } = require("./getReleases");
+const { dialog, app, shell } = require("electron");
+
+module.exports = {
+  checkUpdates: async () => {
+    let response = await getReleases();
+    if (response[0]) {
+      let releases = await response[1];
+      let latest = releases[0];
+
+      if (latest.tag_name !== app.getVersion()) {
+        dialog
+          .showMessageBox({
+            title: "New Update Available",
+            message: `New Version: v${latest.tag_name}`,
+            detail: "Download?",
+            buttons: ["Download", "Cancel"],
+          })
+          .then(({ response }) => {
+            if (response === 0) {
+              let url;
+              switch (process.platform) {
+                case "win32":
+                  url = latest.assets.find((asset) =>
+                    asset.name.includes("exe")
+                  ).browser_download_url;
+                  shell.openExternal(url);
+                  break;
+                case "darwin":
+                  url = latest.assets.find((asset) =>
+                    asset.name.includes("dmg")
+                  ).browser_download_url;
+                  shell.openExternal(url);
+                  break;
+                case "linux":
+                  url = latest.assets.find((asset) =>
+                    asset.name.includes("AppImage")
+                  ).browser_download_url;
+                  shell.openExternal(url);
+                  break;
+                default:
+                  shell.openExternal(latest.html_url);
+                  break;
+              }
+            }
+          })
+          .catch((err) => console.error(err));
+      }
+    } else {
+      dialog.showErrorBox("Error while checking updates", response[1]);
+    }
+  },
+};
