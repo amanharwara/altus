1c328b2eb3f8db6fa6e7fcd5eae9b02d0da085a1
Aman Harwara <amanharwara@protonmail.com>
2021-01-11 23:50:09 +0530

    Connect svelte tab store to electron tab store

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,32 @@
 <script lang="ts">
 	import TabBar from "./components/TabBar.svelte";
 	import TabContent from "./components/TabContent.svelte";
+	import { tabs } from "./store";
+	const Store = require("electron-store");
+
+	let tabStore = new Store({
+		name: "tabs",
+		defaults: {
+			tabs: [],
+		},
+	});
+
+	tabs.set(tabStore.get("tabs"));
+
+	tabs.subscribe((tabs) => {
+		tabStore.set("tabs", tabs);
+	});
 </script>
 
 <style>
+	main {
+		height: 100%;
+	}
+	.container {
+		display: flex;
+		flex-flow: column;
+		height: 100%;
+	}
 </style>
 
 <main>
