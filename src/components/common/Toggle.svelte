adf9d92326f9ec6da77bc550d99b3fd22686c49e
Aman Harwara <amanharwara@protonmail.com>
2021-01-11 23:46:02 +0530

    Create Toggle component

---
+++ src/components/common/Toggle.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let id = "toggle";
+  export let value = false;
+</script>
+
+<style>
+</style>
+
+<div class="toggle">
+  <input type="checkbox" {id} checked={value} />
+  <div class="toggle-bg" />
+</div>
