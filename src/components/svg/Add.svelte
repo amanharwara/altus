0a9e9edb9477ec36733fdb50aa1f07ff47108ee3
Aman Harwara <amanharwara@protonmail.com>
2021-01-11 23:50:38 +0530

    Add "Plus" icon for Add Tab button

---
+++ src/components/svg/Add.svelte
@@ -0,0 +1,8 @@
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  aria-hidden="true"
+  focusable="false"
+  style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);"
+  preserveAspectRatio="xMidYMid meet"
+  viewBox="0 0 24 24"><path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z" /></svg>
