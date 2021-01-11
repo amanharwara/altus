6b0434dc7f3d169c837d6cea5ea7de415cd02060
Aman Harwara <amanharwara@protonmail.com>
2021-01-11 23:45:00 +0530

    Add WhatsApp webview w/ preload

---
+++ src/components/WebView.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let partition;
+</script>
+
+<webview
+  id={`webview-${partition}`}
+  src="https://web.whatsapp.com"
+  preload="../preload.js"
+  {partition}
+  useragent={window.navigator.userAgent.replace(/(Altus4|Electron)([^\s]+\s)/g, '')} />
