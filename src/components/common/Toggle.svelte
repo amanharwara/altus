85a37d3c4afd48eb375c25e0e7a561e3ad179b33
Aman Harwara <amanharwara@protonmail.com>
2021-01-13 00:15:58 +0530

    Add Toggle component

--- src/components/common/Toggle.svelte
+++ src/components/common/Toggle.svelte
@@ -4,9 +4,62 @@
 </script>
 
 <style>
+  .toggle-bg,
+  .toggle {
+    background: #fff;
+    width: 2.75rem;
+    height: 1.5rem;
+    border-radius: 1rem;
+    cursor: pointer;
+    position: relative;
+  }
+
+  .toggle {
+    background: none;
+  }
+
+  .toggle-bg::after {
+    content: "";
+    width: 1.1rem;
+    height: 1.1rem;
+    border-radius: 100%;
+    background: #2a71d1;
+    position: absolute;
+    top: 50%;
+    left: 10%;
+    transform: translateY(-50%);
+    z-index: 0;
+  }
+
+  .checkbox {
+    cursor: pointer;
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 5;
+  }
+
+  .checkbox:checked + .toggle-bg::after {
+    transition: 0.15s;
+    background: #fff;
+    left: auto;
+    right: 10%;
+  }
+
+  .checkbox:checked + .toggle-bg {
+    background: #2a71d1;
+  }
+
+  .checkbox:focus + .toggle-bg,
+  .checkbox:active + .toggle-bg {
+    outline: 1px solid #2a71d1;
+  }
 </style>
 
 <div class="toggle">
-  <input type="checkbox" {id} checked={value} />
+  <input type="checkbox" class="checkbox" {id} checked={value} />
   <div class="toggle-bg" />
 </div>
