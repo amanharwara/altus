b1566a4f1aecb765da68ee25fc4476a738d02400
Aman Harwara <amanharwara@protonmail.com>
2021-01-13 16:11:08 +0530

    SVG Icons

---
+++ src/components/svg/Check.svelte
@@ -0,0 +1,9 @@
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  aria-hidden="true"
+  focusable="false"
+  style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);"
+  preserveAspectRatio="xMidYMid meet"
+  viewBox="0 0 512 512"><path
+    d="M200.359 382.269L61.057 251.673l21.886-23.346l116.698 109.404l229.045-229.044l22.628 22.626l-250.955 250.956z" /></svg>
