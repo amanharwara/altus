a4839c059bf3b72a6f694dac97ac895646e53b04
Aman Harwara <amanharwara@protonmail.com>
2021-01-14 14:18:59 +0530

    Re-format & add Theme Manager

--- src/App.svelte
+++ src/App.svelte
@@ -1,79 +1,58 @@
 <script lang="ts">
-	import type { TabType } from "./types";
-	import TabBar from "./components/TabBar.svelte";
-	import TabConfigModal from "./components/TabConfigModal.svelte";
-	import TabContent from "./components/TabContent.svelte";
-	import { tabs, themes } from "./store";
-	const Store = require("electron-store");
-	const { v4: uuid } = require("uuid");
+  import TabBar from "./components/TabBar.svelte";
+  import TabConfigModal from "./components/TabConfigModal.svelte";
+  import TabContent from "./components/TabContent.svelte";
+  import ThemeManager from "./components/ThemeManager.svelte";
+  import { tabs, themes } from "./store";
+  import defaultTabSettings from "./util/defaultTabSettings";
+  import migrateTab from "./util/migrateTab";
+  const Store = require("electron-store");
+  const { ipcRenderer } = require("electron");
 
-	let defaultTabSettings = {
-		id: uuid(),
-		name: "",
-		active: false,
-		config: {
-			theme: "default",
-			notifications: true,
-			sound: true,
-			utilityBar: false,
-			color: "#2A3440",
-			spellChecker: false,
-		},
-	};
+  let tabConfigModalVisible = false;
+  let themeManagerVisible = false;
 
-	let tabConfigModalVisible = false;
-	let tabSettings = defaultTabSettings;
+  let tabSettings = defaultTabSettings();
 
-	let tabStore = new Store({
-		name: "tabs",
-		defaults: {
-			tabs: [],
-		},
-	});
+  let tabStore = new Store({
+    name: "tabs",
+    defaults: {
+      tabs: [],
+    },
+  });
 
-	let themeStore = new Store({
-		name: "themes",
-		defaults: {
-			themes: [
-				{
-					name: "Default",
-					css: "",
-				},
-				{
-					name: "Dark",
-					css: "",
-				},
-			],
-		},
-	});
+  let themeStore = new Store({
+    name: "themes",
+    defaults: {
+      themes: [
+        {
+          name: "Default",
+          css: "",
+        },
+        {
+          name: "Dark",
+          css: "",
+        },
+      ],
+    },
+  });
 
-	tabs.set(
-		tabStore.get("tabs").map((tab) => {
-			// Migrate v3 tabs to v4
-			if (!tab.config) {
-				return {
-					id: tab.id,
-					name: tab.name,
-					config: {
-						theme: tab.theme,
-						notifications: tab.notifications,
-						sound: tab.sound,
-						utilityBar: tab.utility_bar,
-						color: tab.tab_color,
-						spellChecker: tab.spellcheck,
-					},
-				};
-			} else {
-				return tab;
-			}
-		})
-	);
-	tabs.subscribe((tabs) => tabStore.set("tabs", tabs));
+  tabs.set(tabStore.get("tabs").map(migrateTab));
+  tabs.subscribe((tabs) => {
+    tabStore.set("tabs", tabs);
+    if (tabs.length === 0) {
+      tabConfigModalVisible = true;
+    }
+  });
 
-	themes.set(themeStore.get("themes"));
-	themes.subscribe((themes) => themeStore.set("themes", themes));
+  themes.set(themeStore.get("themes"));
+  themes.subscribe((themes) => themeStore.set("themes", themes));
 
-	/* onMount(() => {
+  ipcRenderer.on("open-theme-manager", () => {
+    themeManagerVisible = true;
+  });
+
+  /* onMount(() => {
 		if (!$themes.find((theme) => theme.name === "Dark Plus")) {
 			window
 				.fetch(
@@ -86,36 +65,44 @@
 	}); */
 </script>
 
-<style>
-	main {
-		height: 100%;
-	}
-	.container {
-		display: flex;
-		flex-flow: column;
-		height: 100%;
-		position: relative;
-	}
-</style>
-
 <main>
-	<div class="container">
-		<TabBar
-			on:add-tab={() => {
-				tabConfigModalVisible = true;
-				tabSettings = defaultTabSettings;
-			}}
-			on:edit-tab={(e) => {
-				tabSettings = e.detail.tabToEdit;
-				tabConfigModalVisible = true;
-			}} />
-		<TabContent />
-		<TabConfigModal
-			visible={tabConfigModalVisible}
-			bind:tabSettings
-			on:close-tab-config-modal={() => {
-				tabConfigModalVisible = false;
-				tabSettings = defaultTabSettings;
-			}} />
-	</div>
+  <div class="container">
+    <TabBar
+      on:add-tab={() => {
+        tabConfigModalVisible = true;
+        tabSettings = defaultTabSettings();
+      }}
+      on:edit-tab={(e) => {
+        tabSettings = e.detail.tabToEdit;
+        tabConfigModalVisible = true;
+      }}
+    />
+    <TabContent />
+    <ThemeManager
+      visible={themeManagerVisible}
+      on:close-theme-manager={() => {
+        themeManagerVisible = false;
+      }}
+    />
+    <TabConfigModal
+      visible={tabConfigModalVisible}
+      {tabSettings}
+      on:close-tab-config-modal={() => {
+        tabConfigModalVisible = false;
+        tabSettings = defaultTabSettings();
+      }}
+    />
+  </div>
 </main>
+
+<style>
+  main {
+    height: 100%;
+  }
+  .container {
+    display: flex;
+    flex-flow: column;
+    height: 100%;
+    position: relative;
+  }
+</style>
