b402c79dc93c7ed9699898f187dc155405251400
Aman Harwara <amanharwara@protonmail.com>
2021-01-14 14:17:27 +0530

    Support tab settings in webview

--- src/components/WebView.svelte
+++ src/components/WebView.svelte
@@ -1,10 +1,38 @@
 <script lang="ts">
+  import { afterUpdate, onMount } from "svelte";
+  import type { TabType } from "../types";
   export let partition;
+  export let tab: TabType;
+
+  let webviewElement;
+  let hasStoppedLoading = false;
+
+  const sendWebviewConfig = () => {
+    webviewElement.send("set-theme", tab.config.theme);
+    webviewElement.setAudioMuted(!tab.config.sound);
+  };
+
+  afterUpdate(() => {
+    if (!hasStoppedLoading) {
+      webviewElement.addEventListener("did-stop-loading", () => {
+        hasStoppedLoading = true;
+        sendWebviewConfig();
+      });
+    } else {
+      sendWebviewConfig();
+    }
+  });
 </script>
 
 <webview
   id={`webview-${partition}`}
   src="https://web.whatsapp.com"
-  preload="../preload.js"
+  preload="../src/preload.js"
   {partition}
-  useragent={window.navigator.userAgent.replace(/(Altus4|Electron)([^\s]+\s)/g, '')} />
+  bind:this={webviewElement}
+  useragent={window.navigator.userAgent.replace(
+    /(Altus4|Electron)([^\s]+\s)/g,
+    ""
+  )}
+  webpreferences={`spellcheck=${tab.config.spellChecker ? 1 : 0}`}
+/>
