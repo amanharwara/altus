5092f18e591c7ff74dcbf408309ae8c2906bb47d
Aman Harwara <amanharwara@protonmail.com>
2021-01-14 14:18:21 +0530

    Add initial theme manager markup

---
+++ src/components/ThemeManager.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  import { fade } from "svelte/transition";
+  import Close from "./svg/close.svelte";
+  export let visible = false;
+
+  const dispatchEvent = createEventDispatcher();
+  const closeThemeManager = () => {
+    dispatchEvent("close-theme-manager");
+  };
+</script>
+
+{#if visible}
+  <div class="modal-container" transition:fade={{ duration: 100 }}>
+    <div class="modal">
+      <div class="header">
+        <div class="title">Theme Manager</div>
+        <button class="close" on:click={() => closeThemeManager()}>
+          <Close />
+        </button>
+      </div>
+    </div>
+    <div class="overlay" on:click={() => closeThemeManager()} />
+  </div>
+{/if}
+
+<style>
+  .modal-container {
+    z-index: 2;
+  }
+  .modal {
+    width: max(300px, 30vw);
+  }
+</style>
