7561afaa6edc2e4cf6ebf8238f6ab5e97c220b72
Aman Harwara <amanharwara@protonmail.com>
2021-01-15 13:22:22 +0530

    Icon SVG

---
+++ src/components/svg/Edit.svelte
@@ -0,0 +1,12 @@
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  aria-hidden="true"
+  focusable="false"
+  style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);"
+  preserveAspectRatio="xMidYMid meet"
+  viewBox="0 0 24 24"
+  ><path
+    d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04a.996.996 0 0 0 0-1.41l-2.34-2.34a.996.996 0 0 0-1.41 0l-1.83 1.83l3.75 3.75l1.83-1.83z"
+  /></svg
+>
