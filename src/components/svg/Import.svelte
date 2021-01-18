ee07ea07a59ffcbc88367124001c03074f055736
Aman Harwara <amanharwara@protonmail.com>
2021-01-18 13:00:32 +0530

    Import/Export icons

---
+++ src/components/svg/Import.svelte
@@ -0,0 +1,17 @@
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  aria-hidden="true"
+  focusable="false"
+  style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);"
+  preserveAspectRatio="xMidYMid meet"
+  viewBox="0 0 36 36"
+  ><path d="M6 22h24v2H6z" />
+  <path d="M26 26h4v2h-4z" />
+  <path
+    d="M13 9.92L17 6v13a1 1 0 1 0 2 0V6l4 3.95a1 1 0 1 0 1.38-1.45L18 2.16L11.61 8.5A1 1 0 0 0 13 9.92z"
+  />
+  <path
+    d="M30.84 13.37A1.94 1.94 0 0 0 28.93 12H21v2h7.95c1.05 2.94 2.77 7.65 3.05 8.48V30H4v-7.52C4.28 21.65 7.05 14 7.05 14H15v-2H7.07a1.92 1.92 0 0 0-1.9 1.32C2 22 2 22.1 2 22.33V30a2 2 0 0 0 2 2h28a2 2 0 0 0 2-2v-7.67c0-.23 0-.33-3.16-8.96z"
+  /></svg
+>
