22ba787e6d5f01e3a37bfea6d71a64e53e69870b
Aman Harwara <amanharwara@protonmail.com>
2021-01-21 13:48:38 +0530

    Update whatsapp preload

--- src/preload.js
+++ src/preload.js
@@ -1,11 +1,6 @@
-const { remote, ipcRenderer } = require("electron");
+const { ipcRenderer } = require("electron");
 
-var ses = remote.session.defaultSession;
-
-ses.flushStorageData();
-ses.clearStorageData({
-  storages: ["appcache", "serviceworkers", "cachestorage", "websql", "indexdb"],
-});
+ipcRenderer.send("flush-session-data");
 
 if (window.navigator.serviceWorker) {
   window.navigator.serviceWorker.getRegistrations().then((registrations) => {
@@ -15,11 +10,81 @@ if (window.navigator.serviceWorker) {
   });
 }
 
+const dispatchMouseEvent = (element, events) => {
+  events.forEach(function (eventName) {
+    var event = new MouseEvent(eventName, {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+      button: 0,
+    });
+    element.dispatchEvent(event);
+  });
+};
+
+const addChatIDs = () => {
+  if (document.querySelectorAll('#pane-side [role="region"] > *').length > 0) {
+    document
+      .querySelectorAll('#pane-side [role="region"] > *')
+      .forEach((chat) => {
+        let internalInstance =
+          chat[
+            Object.keys(chat).find((key) => key.includes("InternalInstance"))
+          ];
+        let id =
+          internalInstance.memoizedProps.children.props.contact.id._serialized;
+        chat.id = id;
+      });
+  }
+};
+
 window.onload = () => {
   const title_element = document.querySelector(".landing-title");
   if (title_element && title_element.innerHTML.includes("Google Chrome")) {
     window.location.reload();
   }
+
+  document.body.addEventListener("click", (e) => {
+    if (
+      e.target.tagName === "A" &&
+      e.target.getAttribute("target") === "_blank"
+    ) {
+      ipcRenderer.send("open-link", e.target.href);
+    }
+  });
+
+  document.body.addEventListener("wheel", (e) => {
+    const delta = Math.sign(e.deltaY);
+    if (e.ctrlKey) {
+      switch (delta) {
+        case -1:
+          ipcRenderer.send("zoom", "in");
+          break;
+        case +1:
+          ipcRenderer.send("zoom", "out");
+          break;
+        default:
+          break;
+      }
+    }
+  });
+
+  document.body.addEventListener("keydown", (e) => {
+    if (e.ctrlKey) {
+      switch (e.key) {
+        case "=":
+        case "+":
+          ipcRenderer.send("zoom", "in");
+          break;
+        case "-":
+          ipcRenderer.send("zoom", "out");
+          break;
+        case "0":
+          ipcRenderer.send("zoom", "reset");
+          break;
+      }
+    }
+  });
 
   new MutationObserver(function (mutations) {
     let title = mutations[0].target.innerText;
@@ -38,6 +103,29 @@ window.onload = () => {
     subtree: true,
     childList: true,
     characterData: true,
+  });
+
+  new MutationObserver((mutations) => {
+    // Check when WhatsApp is done loading
+    if (
+      mutations[0].removedNodes &&
+      mutations[0].removedNodes[0].id === "startup"
+    ) {
+      addChatIDs();
+
+      // Update chat IDs when a chat is added/removed.
+      new MutationObserver(() => {
+        addChatIDs();
+      }).observe(
+        document.querySelector('#pane-side [role="region"]', {
+          subtree: true,
+          childList: true,
+        })
+      );
+    }
+  }).observe(document.querySelector("#app"), {
+    subtree: true,
+    childList: true,
   });
 };
 
@@ -83,4 +171,57 @@ ipcRenderer.on("set-theme", (e, theme) => {
   }).observe(document.body, {
     attributes: true,
   });
+});
+
+ipcRenderer.on("set-utility-bar", (e, value) => {});
+
+ipcRenderer.on("toggle-notifications", (_, setting) => {
+  if (!setting) {
+    window.Notification = "";
+  } else {
+    // Proxy the original Notification constructor
+    let NativeNotification = Notification;
+
+    // Replace the original Notification constructor with custom one
+    Notification = function (title, options) {
+      let notification = new NativeNotification(title, options);
+
+      notification.addEventListener("click", function () {
+        // Maximize the window and activate the tab that the message was in.
+        ipcRenderer.send(
+          "activate-window-and-tab",
+          document.body.dataset.tabid
+        );
+
+        // Simulate click on the particular chat element to open it.
+        let chat = document
+          .getElementById(options.tag)
+          .querySelector('[role="option"] > * > :last-child');
+        dispatchMouseEvent(chat, [
+          "mouseover",
+          "mousedown",
+          "mouseup",
+          "click",
+        ]);
+      });
+
+      notification.addEventListener = function () {
+        return true;
+      };
+      notification.attachEvent = function () {
+        return true;
+      };
+      notification.addListener = function () {
+        return true;
+      };
+
+      return notification;
+    };
+
+    Notification.prototype = NativeNotification.prototype;
+    Notification.permission = NativeNotification.permission;
+    Notification.requestPermission = NativeNotification.requestPermission.bind(
+      Notification
+    );
+  }
 });
