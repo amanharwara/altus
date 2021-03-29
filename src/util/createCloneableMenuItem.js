142ccaabbc8fd14aa6218c8155d70da8b6efd68a
Aman Harwara <amanharwara@protonmail.com>
2021-03-30 00:11:39 +0530

    Utility function to clone MenuItem through IPC

---
+++ src/util/createCloneableMenuItem.js
@@ -0,0 +1,25 @@
+const createCloneableMenuItem = ({
+  submenu,
+  id,
+  label,
+  type,
+  accelerator,
+  commandId,
+}) => {
+  let newSubmenu;
+
+  if (submenu?.items && submenu?.items.length > 0) {
+    newSubmenu = submenu.items.map((item) => createCloneableMenuItem(item));
+  }
+
+  return {
+    ...(id ? { id } : {}),
+    ...(label ? { label } : {}),
+    ...(newSubmenu && newSubmenu.length > 0 ? { submenu: newSubmenu } : {}),
+    ...(type ? { type } : {}),
+    ...(accelerator ? { accelerator } : {}),
+    ...(commandId ? { commandId } : {}),
+  };
+};
+
+module.exports = createCloneableMenuItem;
