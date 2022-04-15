098eb7292bd3fdc0f954a48bc4b45320c70c6331
Aman Harwara <amanharwara@protonmail.com>
2022-04-15 15:42:24 +0530

    feat: only clear service workers when flusing session data

--- src/ipcHandlers/main/flushSessionData.js
+++ src/ipcHandlers/main/flushSessionData.js
@@ -2,13 +2,7 @@ const flushSessionData = ({ sender }) => {
   let session = sender.session;
   session.flushStorageData();
   session.clearStorageData({
-    storages: [
-      "appcache",
-      "serviceworkers",
-      "cachestorage",
-      "websql",
-      "indexdb",
-    ],
+    storages: ["serviceworkers"],
   });
 };
 
