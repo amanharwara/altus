842b7712bfae40617c9e2577ce5dfae8337d5ec4
Aman Harwara <amanharwara@protonmail.com>
2023-09-03 20:49:01 +0530

    webview types

---
+++ src/webview.d.ts
@@ -0,0 +1,13 @@
+// Add <webview> tag support to JSX
+
+import { type WebviewTag } from "electron";
+
+declare module "solid-js" {
+  namespace JSX {
+    interface IntrinsicElements {
+      webview: Partial<WebviewTag> & {
+        class?: string;
+      };
+    }
+  }
+}
