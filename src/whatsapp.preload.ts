ad9f2626593cc91bca83bf2b2ca011612f6668bc
Aman Harwara <amanharwara@protonmail.com>
2023-09-08 01:02:51 +0530

    apply theme css

--- src/whatsapp.preload.ts
+++ src/whatsapp.preload.ts
@@ -11,10 +11,24 @@ window.onload = () => {
   document.body.className = "web";
 };
 
+function setThemeCSS(css: string) {
+  const existingStyle = document.getElementById("altus-style");
+  if (existingStyle) {
+    existingStyle.innerHTML = css;
+  } else {
+    const styleElement = document.createElement("style");
+    styleElement.id = "altus-style";
+    styleElement.innerHTML = css;
+    document.head.appendChild(styleElement);
+  }
+}
+
 ipcRenderer.on("set-theme", (event, theme: Theme) => {
   if (theme.id === "dark") {
     document.body.classList.add("dark");
   } else if (theme.id === "default") {
     document.body.classList.remove("dark");
   }
+
+  setThemeCSS(theme.css ? theme.css : "");
 });
