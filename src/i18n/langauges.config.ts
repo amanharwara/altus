9fc73d6b3c14234b94587dcfd9b7cd35139b9d80
EFEFIX <druy7004@gmail.com>
2024-05-21 15:43:28 +0300

    Turkish language added.

--- src/i18n/langauges.config.ts
+++ src/i18n/langauges.config.ts
@@ -1,3 +1,3 @@
-export const languages = ["en", "es", "hi", "it", "pt", "de"] as const;
+export const languages = ["en", "es", "hi", "it", "pt", "de", "tr"] as const;
 export type Language = (typeof languages)[number];
 export const fallbackLanguage = "en";
