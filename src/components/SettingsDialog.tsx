453830b51bf313a5d9f9d365a9e3d99b6104ec54
Param Siddharth <contact@paramsid.com>
2026-05-07 01:50:58 +0530

    Add setting to toggle registering as the default handler for whatsapp:// links
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- src/components/SettingsDialog.tsx
+++ src/components/SettingsDialog.tsx
@@ -52,6 +52,20 @@ const SettingsDialog: Component<{
               </div>
               <div class="py-2.5">
                 <StyledSwitch
+                  checked={getSettingValue("registerProtocol")}
+                  onChange={(checked) => setSettingValue("registerProtocol", checked)}
+                  class="items-start"
+                >
+                  <div class="flex flex-col gap-1.5">
+                    <div class="font-semibold">{t("registerProtocol")}</div>
+                    <div class="text-zinc-300 max-w-[30ch] leading-snug text-sm">
+                      {t("registerProtocolDescription")}
+                    </div>
+                  </div>
+                </StyledSwitch>
+              </div>
+              <div class="py-2.5">
+                <StyledSwitch
                   checked={getSettingValue("tabBar")}
                   onChange={(checked) => setSettingValue("tabBar", checked)}
                   class="items-start"
